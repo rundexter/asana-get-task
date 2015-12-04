7b9195a02212eec4e09d1ddda86a16b88e47537b
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-04 14:56:46 +0200

    fix

--- index.js
+++ index.js
@@ -82,6 +82,17 @@ module.exports = {
         request[method]({url: api, form: options, auth: auth, json: true}, callback);
     },
 
+    prepareStringInputs: function (inputs, inputAttributes) {
+        var result = {};
+
+        _.map(_.pick(inputs, inputAttributes), function (inputValue, inputKey) {
+
+            result[inputKey] = _(inputValue).toString();
+        });
+
+        return result;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
